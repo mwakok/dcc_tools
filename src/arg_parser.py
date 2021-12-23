f995c8e937f077ba80f25dc411aba918e9084dda
Maurits Kok <m.w.a.kok@tudelft.nl>
2021-12-23 19:48:54 +0100

    Create CLI call

---
+++ src/arg_parser.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+
+
+def create_parser():
+    """Create a CLI argument parser
+
+    Returns
+    -------
+    args
+        defined arguments for CLI
+    """
+    parser = argparse.ArgumentParser(formatter_class=argparse.MetavarTypeHelpFormatter,)
+
+    parser.add_argument(
+        "--repo", type=str, help="name of the GitHub repository", required=True,
+    )
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--project", default="DCC Support", type=str)
+    parser.add_argument("--owner", default=os.environ["GITHUB_USER"], type=str)
+    parser.add_argument("--token", default=os.environ["GITHUB_TOKEN"], type=str)
+    parser.add_argument(
+        "--columns", default=["To do", "In progress", "Done"], type=list
+    )
+    parser.add_argument("--issue_column", default="To do", type=str)
+    parser.add_argument("--loglevel", default="WARNING", type=str)
+    args = parser.parse_args()
+
+    return args
---
+++ src/upload_project.py
@@ -0,0 +1,42 @@
+from arg_parser import create_parser
+
+from logging_functions import _init_logger, set_logger_level
+from GithubAPI import GitHubAPI
+
+_init_logger()
+
+
+def main(
+    repo_name,
+    path_issues,
+    project_name,
+    repo_owner,
+    token,
+    columns,
+    issue_column,
+    loglevel,
+):
+
+    set_logger_level(loglevel)
+
+    repo = GitHubAPI(repo_name, repo_owner, token)
+    repo.load_markdown_files(path_issues)
+    repo.push_project(project_name, columns)
+    repo.push_issues()
+    repo.add_issues_to_project(project_name, issue_column)
+
+
+if __name__ == "__main__":
+
+    args = create_parser()
+
+    main(
+        repo_name=args.repo,
+        path_issues=args.path,
+        project_name=args.project,
+        repo_owner=args.owner,
+        token=args.token,
+        columns=args.columns,
+        issue_column=args.issue_column,
+        loglevel=args.loglevel,
+    )
