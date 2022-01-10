5c6e62cb7d67874cb5e474555603cb200cb42d12
Maurits Kok <m.w.a.kok@tudelft.nl>
2022-01-10 14:32:26 +0100

    Add default values to help

--- ghproject/arg_parser.py
+++ ghproject/arg_parser.py
@@ -18,31 +18,36 @@ def create_parser():
         "--repo", type=str, required=True, help="name of the GitHub repository"
     )
     parser.add_argument(
-        "--path", type=str, help="path to the folder with markdown files"
+        "--path", type=str, required=True, help="path to the folder with markdown files"
     )
     parser.add_argument(
-        "--project", default="DCC Support", type=str, help="name of the project board"
+        "--project",
+        default="DCC Support",
+        type=str,
+        help="name of the project board, default is 'DCC Support'",
     )
     parser.add_argument(
         "--owner",
         default=os.environ["GITHUB_USER"],
         type=str,
-        help="GitHub username or organization name",
+        help="GitHub username or organization name, default is environment variable GITHUB_USER",
     )
     parser.add_argument(
         "--token",
         default=os.environ["GITHUB_TOKEN"],
         type=str,
-        help="GitHub Personal Access Token",
+        help="GitHub Personal Access Token, default is environment variable GITHUB_TOKEN",
     )
     parser.add_argument(
         "--columns",
         default=["To do", "In progress", "Done"],
         type=list,
-        help="list of column names in project board",
+        help="list of column names in project board, default is ['To do', 'In progress', 'Done']",
     )
     parser.add_argument(
-        "--issue_column", type=str, help="project board column to add issues to"
+        "--issue_column",
+        type=str,
+        help="project board column to add issues to, default is first element in list columns",
     )
     parser.add_argument("--loglevel", default="WARNING", type=str, help="loglevels")
     args = parser.parse_args()
